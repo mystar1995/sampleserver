75e4ecc3d3d43ca66fc8a7d222ef2e99cb4ac3b1
mystar1995 <xixian95@outlook.com>
2020-08-05 07:52:10 +0800

    update

--- convert.sh
+++ convert.sh
@@ -19,6 +19,6 @@ $januspprec_binary $session_prefix-audio.mjr $tmp_audio
 
 echo "Merging audio track with video ..."
 
-ffmpeg -i $tmp_audio -i $tmp_video -filter_complex "transpose=1"  -c:v copy -c:a opus $output_file
+ffmpeg -i $tmp_audio -i $tmp_video -filter_complex "transpose=1" -map 0:a -c:a opus $output_file
 
 echo "Done !"
