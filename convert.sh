c643b5eeea59605de362bdecaabb9742d19fa159
mystar1995 <xixian95@outlook.com>
2020-08-05 23:30:29 +0800

    update

--- convert.sh
+++ convert.sh
@@ -19,7 +19,7 @@ $januspprec_binary $session_prefix-audio.mjr $tmp_audio
 
 echo "Merging audio track with video ..."
 
-ffmpeg -i $tmp_audio -i $tmp_video -filter_complex "transpose=2" -map 0:a -c:a opus -strict experimental $output_file
+ffmpeg -i $tmp_audio -i $tmp_video -filter_complex "transpose=2" -map 0:a -c:a aac -strict experimental $output_file
 
 
 echo "Done !"
